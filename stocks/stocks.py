f82fcd1cf299a00e6ef50c0eb29ba6905e24d74d
ItayBoros <itaybor15@gmail.com>
2025-02-19 11:15:36 +0200

    Update stocks.py

--- stocks/stocks.py
+++ stocks/stocks.py
@@ -20,9 +20,14 @@ client = pymongo.MongoClient("mongodb://mongo:27017/")
 db = client[db_name]  # db_name must be a string
 inv = db["inventory"]
 
-Stocks = {}
 def genID():
     return str(uuid.uuid4())
+
+def transform_stock(stock):
+    if not stock:
+        return stock
+    stock["id"] = stock.pop("_id")
+    return stock
 
 @app.route('/kill', methods=['GET'])
 def kill_container():
@@ -45,61 +50,60 @@ def addStock():
         data = request.get_json()
         required_fields = ['symbol', 'purchase price', 'shares']
 
-        #check if there is Malformed data
+        # Check for malformed data
         if not all(field in data for field in required_fields):
             return jsonify({"error": "Malformed data"}), 400
         
-        #check if symbol is not a string
+        # Check if symbol is not a string
         if not isinstance(data['symbol'], str):
-                return jsonify({"error": "Invalid stock symbol"}), 400
+            return jsonify({"error": "Invalid stock symbol"}), 400
         
-        # check if shares is not a positive integer
+        # Check if shares is not a positive integer
         if not isinstance(data['shares'], int) or data['shares'] <= 0:
             return jsonify({"error": "Shares must be a positive integer"}), 400
 
-        # check if purchase price is not a positive number
+        # Check if purchase price is not a positive number
         if not isinstance(data['purchase price'], (int, float)) or data['purchase price'] <= 0:
             return jsonify({"error": "Purchase price must be a positive number"}), 400
-        # generate UUID
+
+        # Generate UUID
         new_id = genID()
 
-        # check if name field enter in the POST
-        if 'name' not in data:
-            name = "NA"
-        else:
+        # Check if the optional name field is provided and valid
+        if 'name' in data:
             if not isinstance(data['name'], str):
                 return jsonify({"error": "name must be a string"}), 400
             name = data['name']
+        else:
+            name = "NA"
 
-        # check if purchase date field enter in the POST
+        # Check if stock already exists for this account (by symbol)
         doc = inv.find_one({"symbol": data['symbol'].upper()})
         if doc:
             return jsonify({"error": "Stock symbol already exists for this account"}), 400
 
-        # Set optional fields
-        name = data.get('name', "NA")
+        # Set optional purchase date field
         purchase_date = data.get('purchase date', "NA")
         if purchase_date != "NA" and not validate_date_format(purchase_date):
             return jsonify({"error": "Invalid date format. Use DD-MM-YYYY"}), 400
 
-        stock = {'_id': new_id,
-                 'name': name,
-                 'symbol': data['symbol'].upper(),
-                 'purchase price': round(data['purchase price'], 2),
-                 'purchase date': purchase_date,
-                 'shares': data['shares']
-                }
+        stock = {
+            '_id': new_id,
+            'name': name,
+            'symbol': data['symbol'].upper(),
+            'purchase price': round(data['purchase price'], 2),
+            'purchase date': purchase_date,
+            'shares': data['shares']
+        }
         inv.insert_one(stock)
-        response_data = {'_id': new_id}
+        response_data = {"id": new_id}
         return jsonify(response_data), 201
     except Exception as e:
         return jsonify({"server error": str(e)}), 500
 
-
 @app.route('/stocks', methods=['GET'])
 def getStocks():
     try:
-        #moves the filters into dic
         query = request.args.to_dict()
 
         for key in query:
@@ -108,56 +112,53 @@ def getStocks():
             elif key == "purchase price":
                 query[key] = float(query[key])
         
-        #in case there is no filters, return all
+        # If there are no filters, return all stocks
         if not query:
             all_stocks = list(inv.find())
-            return jsonify(all_stocks), 200
+            # Transform each stock document
+            transformed = [transform_stock(stock) for stock in all_stocks]
+            return jsonify(transformed), 200
+
         for field in query.keys():
-            if field not in ['_id','name','symbol','shares', 'purchase price', 'purchase date']:
+            if field not in ['id', '_id', 'name', 'symbol', 'shares', 'purchase price', 'purchase date']:
                 return jsonify({'error': 'invalid query field'}), 422
 
-        # Fetch filtered results
         stocks = inv.find(query)
-
-        # Convert cursor to list
         filtered_stocks = list(stocks)
 
-        #in case there is no filtered items
         if not filtered_stocks:
             return jsonify({"error": "No stocks match the given filters"}), 404
 
-        return jsonify(filtered_stocks), 200
+        # Transform each stock document
+        transformed = [transform_stock(stock) for stock in filtered_stocks]
+        return jsonify(transformed), 200
 
     except Exception as e:
         return jsonify({"server error": str(e)}), 500
-
 
 @app.route('/stocks/<string:stockId>', methods=['GET'])
 def getStock(stockId):
     try:
         stock = inv.find_one({'_id': stockId})
         if stock is None:
-            # If there's no matching document, return a 404
             return jsonify({"error": "No such ID"}), 404
-        return jsonify(stock), 200
+        return jsonify(transform_stock(stock)), 200
     except Exception as e:
         return jsonify({"server error": str(e)}), 500
 
 @app.route('/stocks/<string:stockId>', methods=['DELETE'])
 def deleteStock(stockId):
     try:
-        # try to get the stock that want to delete using their id
         stock = inv.find_one({'_id': stockId})
         if not stock:
             return jsonify({"error": "No such ID"}), 404
         resp = inv.delete_one({'_id': stockId})
-        if resp.deleted_count == 1:  # deleted
+        if resp.deleted_count == 1:
             return '', 204
     except KeyError:
         return jsonify({"error": "No such ID"}), 404
     except Exception as e:
         return jsonify({"server error": str(e)}), 500
-
 
 @app.route('/stocks/<string:stockId>', methods=['PUT'])
 def updateStock(stockId):
@@ -167,52 +168,49 @@ def updateStock(stockId):
             return jsonify({"error": "expected application/json media type"}), 415
         data = request.get_json()
 
-        #Check if required fields are present (all fields must appear in the request)
         required_fields = ['_id', 'name', 'symbol', 'purchase price', 'purchase date', 'shares']
         if not all(field in data for field in required_fields):
             return jsonify({"error": "Malformed data"}), 400
 
         stock = inv.find_one({'_id': stockId})
-        # changing ID isn't possible
-        curr_id = stock['_id']
-        if data['_id'] != curr_id:
-            return jsonify({"error": "Stock ID can not be change"}),400
-        
-        # changing symbol isn't possible
-        symbol = stock['symbol']
-        if not isinstance(data['symbol'],str):
+        if not stock:
+            return jsonify({"error": "Not found"}), 404
+
+        # Changing the ID isn't allowed
+        if data['_id'] != stock['_id']:
+            return jsonify({"error": "Stock ID can not be change"}), 400
+
+        # Changing the symbol isn't allowed
+        if not isinstance(data['symbol'], str):
             return jsonify({"error": "Invalid stock symbol"}), 400
-        if data['symbol'].upper() != symbol:
-            return jsonify({"error": "Stock symbol can not be change"})
+        if data['symbol'].upper() != stock['symbol']:
+            return jsonify({"error": "Stock symbol can not be change"}), 400
 
-        # validating data format
-        if not isinstance(data['shares'], int) or (data['shares'] <= 0):
+        # Validate shares and purchase price
+        if not isinstance(data['shares'], int) or data['shares'] <= 0:
             return jsonify({"error": "Shares must be a positive integer"}), 400
-
         if not isinstance(data['purchase price'], (int, float)) or data['purchase price'] <= 0:
             return jsonify({"error": "Purchase price must be a positive number"}), 400
 
-        # adding a name field
+        # Validate and set name field
         if stock['name'] == "NA" and data['name'] != "NA":
             if not isinstance(data['name'], str):
                 return jsonify({"error": "name must be a string"}), 400
-        # name field is already exits but was not updated, the current name remains the same
-        if stock['name'] != "NA" and data['name'] == "NA":
+            name = data['name']
+        elif stock['name'] != "NA" and data['name'] == "NA":
             name = stock['name']
         else:
             if not isinstance(data['name'], str):
                 return jsonify({"error": "name must be a string"}), 400
             name = data['name']
 
-        # adding a date field
+        # Validate and set purchase date field
         if stock['purchase date'] == "NA" and data['purchase date'] != "NA":
             if not validate_date_format(data['purchase date']):
                 return jsonify({"error": "Invalid date format. Use DD-MM-YYYY"}), 400
             purchase_date = data['purchase date']
-        # date field is already exits but was not updated, the current date remains the same
         elif stock['purchase date'] != "NA" and data['purchase date'] == "NA":
             purchase_date = stock['purchase date']
-        # adding date field
         elif stock['purchase date'] != "NA" and data['purchase date'] != "NA":
             if not validate_date_format(data['purchase date']):
                 return jsonify({"error": "Invalid date format. Use DD-MM-YYYY"}), 400
@@ -220,38 +218,29 @@ def updateStock(stockId):
         else:
             purchase_date = stock['purchase date']
 
-        found = inv.find_one({'_id': stockId})
-        # ID isn't valid
-        if not found:
-            return jsonify({"error": "Not found"}), 404
-
-        updated_fields = {'name': name,
-                 'purchase price': round(data['purchase price'], 2),
-                 'purchase date': purchase_date,
-                 'shares': data['shares']
-                 }
+        updated_fields = {
+            'name': name,
+            'purchase price': round(data['purchase price'], 2),
+            'purchase date': purchase_date,
+            'shares': data['shares']
+        }
 
         inv.update_one({'_id': stockId}, {'$set': updated_fields})
-        response_data = {'_id': stockId}
+        response_data = {"id": stockId}
         return jsonify(response_data), 200
 
     except Exception as e:
         return jsonify({"server error": str(e)}), 400
 
-
 def validate_date_format(date_string):
-    #build a valid pattern to date input
     pattern = r"^\d{2}-\d{2}-\d{4}$"
     if not re.match(pattern, date_string):
         return False
     try:
-        #attempt to parse the date with the expected format
         datetime.strptime(date_string, "%d-%m-%Y")
         return True
     except ValueError:
-        #raised when the format does not match
         return False
-
 
 def get_ticker_price(symbol):
     try:
@@ -259,7 +248,6 @@ def get_ticker_price(symbol):
         headers = {'X-Api-Key': 'JtneO16sSFCqKQ8bJqYLEA==yqfESh50NJdz5O5Q'}
         response = requests.get(api_url, headers=headers)
 
-        #make an api call using symbol
         if response.status_code == requests.codes.ok:
             data = response.json()
             return data.get('price')
@@ -270,7 +258,6 @@ def get_ticker_price(symbol):
         print(f"Unexpected error: {e}")
         return None
 
-
 @app.route('/stock-value/<string:stockId>', methods=['GET'])
 def get_stock_value(stockId):
     try:
@@ -278,12 +265,10 @@ def get_stock_value(stockId):
         if not stock:
             return jsonify({"error": "Not found"}), 404
 
-        #retrieve the current ticker price
         ticker_price = get_ticker_price(stock['symbol'])
         if ticker_price is None:
             return jsonify({"error": "Failed to retrieve ticker price"}), 500
 
-        #calculate the stock value
         stock_value = ticker_price * stock['shares']
         return jsonify({
             "symbol": stock['symbol'],
@@ -294,26 +279,18 @@ def get_stock_value(stockId):
     except Exception as e:
         return jsonify({"server error": str(e)}), 500
 
-
 @app.route('/portfolio-value', methods=['GET'])
 def get_portfolio_value():
     try:
         portfolio_value = 0.0
-
-        # iterate over all stocks in the portfolio
         stocks = inv.find()
-        for stock in stocks: # fetch the current ticker price for the stock
+        for stock in stocks:
             ticker_price = get_ticker_price(stock['symbol'])
             if ticker_price is None:
                 return jsonify({"error": "Failed to retrieve ticker price"}), 500
-
-        # calculate the stock value and add it to the portfolio value
             portfolio_value += ticker_price * stock['shares']
 
-        # get the current date
         current_date = datetime.now().strftime('%Y-%m-%d')
-
-        # return the portfolio value and date
         return jsonify({
             "date": current_date,
             "portfolio value": portfolio_value
@@ -321,7 +298,6 @@ def get_portfolio_value():
 
     except Exception as e:
         return jsonify({"server error": str(e)}), 500
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=False)
